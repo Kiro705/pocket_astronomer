14284e7e6f6d2dbfe9c36899dec327a59a9f0787
Jackson Sui <kiro705@gmail.com>
2017-12-06 20:35:08 -0500

    styled results list

--- components/ResultItem.js
+++ components/ResultItem.js
@@ -1,43 +1,50 @@
 import React from 'react'
 import { StyleSheet, View, Image, Text } from 'react-native'
+const Dimensions = require('Dimensions')
+
+const  {height, width} = Dimensions.get('window')
 
 const styles = StyleSheet.create({
-	Container: {
-		height: 500,
-		backgroundColor: '#191919',
-		alignItems: 'center',
-		marginTop: 10,
-		marginLeft: 10,
-		marginRight: 10,
-    marginBottom: 10,
-  },
-  Image: {
-    height: '70%',
-    width: '100%',
-  },
   Text: {
     fontSize: 28,
     fontFamily: 'Courier New',
     textAlign: 'left',
-    backgroundColor: 'rgba(0,0,0,0)',
+    backgroundColor: '#191919',
     color: 'red',
-    margin: 10,
+    marginTop: 10,
+    marginLeft: 10,
+    marginRight: 10,
+    paddingTop: 5,
+    paddingBottom: 5,
+    paddingLeft: 10,
+    paddingRight: 10,
   },
-  Buffer1: {
-    flex: 1,
+  Image: {
+    height: 300,
+    resizeMode: 'contain',
+    width: width - 20,
+    marginLeft: 10,
+    marginRight: 10,
+    backgroundColor: '#191919',
   },
-  Buffer3: {
-    flex: 3,
+  Buffer: {
+    height: 10,
+    width: width - 20,
+    backgroundColor: '#191919',
+    marginLeft: 10,
+    marginRight: 10,
+    marginBottom: 10,
   }
 })
 
 const ResultItem = (props) => {
-	return (
-		<View style={styles.Container}>
-			<Text style={styles.Text}>{props.data.data[0].title}</Text>
-			<Image style={styles.Image} source={{uri: props.data.links[0].href}} />
-		</View>
-	)
+  return (
+    <View>
+      <Text style={styles.Text}>{props.data.data[0].title}</Text>
+      <Image style={styles.Image} source={{uri: props.data.links[0].href}} />
+      <View style={styles.Buffer} />
+    </View>
+  )
 }
 
 export default ResultItem
