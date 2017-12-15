363ac4a2a6488d708ba3ba013dfab3d8eaf64572
Jackson Sui <kiro705@gmail.com>
2017-12-15 14:44:54 -0500

    add the SUN

--- assets/data/stars.js
+++ assets/data/stars.js
@@ -2,9 +2,23 @@ const starsObj = {
   title: 'Stars',
   content: [
     {
-      title: 'Test Star',
-      Mass: '???',
-    },
+      title: 'Sun',
+      Type: 'G2V',
+      Mass: '333,000 Earths',
+      Volume: '1,300,000 Earths',
+      Radius: '109 Earth radii',
+      Density: '1.408 g/cm^3',
+      'Core density': '162.2 g/cm^3',
+      Age: '~4.6 billion years',
+      Luminosity: '3.828×10^26 W',
+      'Visual brightness': '-26.74',
+      'Temperature (photosphere/center)': '9930/28,300,000 °F',
+      'Surface gravity': '27.94 g',
+      'Escape velocity': '617.7 km/s',
+      'Distance from Milky Way core': '27,200 ly',
+      'Rotation period': '25.05 days',
+      Planets: 8,
+      },
   ]
 }
 
